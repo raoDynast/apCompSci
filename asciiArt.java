be27e676b1d2d234a0f1c17b73c8927382821698
raoDynast <89149522+raoDynast@users.noreply.github.com>
2021-08-20 09:26:12 -0600

    Create asciiArt.java
    ascii art assignment.

---
+++ asciiArt.java
@@ -0,0 +1,38 @@
+// Ahmad Rao
+// Period-2 - Ap computer science
+// 20th - August - 2021
+
+
+
+public class asciiArt {
+
+
+public static void main(String[] args) {
+
+    // My ascii art is the map of the United States with the American flag in it!
+
+    System.out.println("                                                                ..");
+    System.out.println("    * * * * * * * * * * $$$$$$$$$$$$$$$$$$$$                 .$$$$.");
+    System.out.println("     * * * * * * * * * * $$$$$$$$$$$$$$$$$$$$$$$$.          .$$$$$");
+    System.out.println("    * * * * * * * * * * ::::::::::::::::::::::::::.      .::::::::      ");
+    System.out.println("     * * * * * * * * * * $$$$$$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$F      ");
+    System.out.println("    * * * * * * * * * * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$d$$$$$$$      ");
+    System.out.println("    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;");
+    System.out.println("    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$      ");
+    System.out.println("    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$      ");
+    System.out.println("    :::::::::::::::::::::::::::::::::::::::::::::::::::::::::;      ");
+    System.out.println("     ^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;      ");
+    System.out.println("       ^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$      ");
+    System.out.println("         ^:::::::::::::::::::::::::::::::::::::::::::::::;      ");
+    System.out.println("           ^^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P      ");
+    System.out.println("                       $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$L      ");
+    System.out.println("                         ;; ;::::::::::::::::;;    ;;:::.      ");
+    System.out.println("                              $$$$$$^     ^^         $$$$$;");
+    System.out.println("                               ^$$^                   $$$$");
+    System.out.println("                                                        $$      ");
+      
+      }
+      
+}
+    
+
